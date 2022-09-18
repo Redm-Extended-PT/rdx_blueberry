13a6c0e41f7d8ec5accd7a62bfaf58e56822b6e9
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-18 18:24:50 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,25 +1,19 @@
-data = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    data = call
-end)
+RDX = nil
+TriggerEvent('rdx:getSharedObject', function(obj) RDX = obj end)
 
-RegisterServerEvent('redemrp_blueberry:addBlueBerry')
-AddEventHandler('redemrp_blueberry:addBlueBerry', function() 
-	local _source = source
-	local ItemData = data.getItem(_source, 'blueberry')
-	local ItemData2 = data.getItem(_source, 'stick')
+RegisterServerEvent('rdx:addBlueBerry')
+AddEventHandler('rdx:addBlueBerry', function() 
+	local xPlayer = RDX.GetPlayerFromId(source)
 	math.randomseed(GetGameTimer())
 	local amount = math.random(1,2)
-	ItemData.AddItem(amount)
-	ItemData2.AddItem(1)
+	
+	xPlayer.addInventoryItem("item_blueberry", 1)  	
+        xPlayer.removeInventoryItem("item_stick", 1)
+	xPlayer.removeInventoryItem("item_cricket", 1)
 end)
 
-
-RegisterServerEvent("RegisterUsableItem:blueberry")
-AddEventHandler("RegisterUsableItem:blueberry", function(source)
-    local _source = source
-	local ItemData = data.getItem(_source, 'blueberry')
-	ItemData.RemoveItem(1)
-    TriggerClientEvent('redemrp_blueberry:EatBlueBerry', _source)
+RDX.RegisterUsableItem("item_blueberry", function(source)
+   local xPlayer = RDX.GetPlayerFromId(source)   
+   xPlayer.removeInventoryItem("item_blueberry", 1)   
+   TriggerClientEvent("rdx:EatBlueBerry", xPlayer)		
 end)
-
